736b9740014671139e80d5159add222602879db5
kimjaeik95 <wodlrdlsla@naver.com>
2024-10-05 03:40:59 +0900

    feat:회원가입,회원삭제,로그인 구현

---
+++ src/main/java/com/example/demo/member/dto/MemberSignInRequest.java
@@ -0,0 +1,12 @@
+package com.example.demo.member.dto;/**
+ *packageName    : com.example.demo.member.dto
+ * fileName       : MemberSignInRequest
+ * author         : JAEIK
+ * date           : 10/5/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 10/5/24        JAEIK       최초 생성
+ */public record MemberSignInRequest() {
+}
