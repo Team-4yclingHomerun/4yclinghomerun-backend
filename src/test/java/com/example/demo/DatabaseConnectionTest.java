76773d8958eda5a4982453bf126c9943bd243a2d
kimjaeik95 <wodlrdlsla@naver.com>
2024-10-16 07:04:01 +0900

    fix: ServerDb JPA 오류 수정중

---
+++ src/test/java/com/example/demo/DatabaseConnectionTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;/**
+ *packageName    : com.example.demo
+ * fileName       : DatabaseConnectionTest
+ * author         : JAEIK
+ * date           : 10/16/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 10/16/24        JAEIK       최초 생성
+ */public class DatabaseConnectionTest {
+}
