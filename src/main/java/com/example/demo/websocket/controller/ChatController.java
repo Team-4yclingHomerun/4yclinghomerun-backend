c5e1abc5215804c4501bed28de6fe81d6d83f892
kimjaeik95 <wodlrdlsla@naver.com>
2026-01-09 23:46:41 +0900

    chore: 부하 테스트용 TEST_MODE 추가 (테스트 환경은 토큰 제거)

--- src/main/java/com/example/demo/websocket/controller/ChatController.java
+++ src/main/java/com/example/demo/websocket/controller/ChatController.java
@@ -22,6 +22,7 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 import java.util.Optional;
+import java.util.UUID;
 
 
 /**
@@ -46,30 +47,40 @@ public class ChatController {
     private final OauthMemberRepository oauthMemberRepository;
     private final MemberRepository memberRepository;
     private final JwtParser jwtParser;
+    private boolean TEST_MODE = true;
 
     @MessageMapping("/chat/message")
     public void message(ChatMessageRequest message, SimpMessageHeaderAccessor headerAccessor) {
         log.debug("message:{}", message);
-        String token = headerAccessor.getFirstNativeHeader(JwtProperties.ACCESS_TOKEN_HEADER);
 
-        if (token != null && token.startsWith(JwtProperties.ACCESS_TOKEN_PREFIX)) {
-            token = token.substring(JwtProperties.ACCESS_TOKEN_PREFIX.length());
+        String nickname;
+        UUID memberId;
+
+        // 부하테스트 토큰 확인 X  (부하테스트 완료 후 성능개선하면 코드지울 예정)
+        if (TEST_MODE) {
+            nickname = "VU-" + headerAccessor.getSessionId(); // VU별 고유 nickname
+            memberId = UUID.randomUUID(); // 테스트용 ID
+        } else {
+            String token = headerAccessor.getFirstNativeHeader(JwtProperties.ACCESS_TOKEN_HEADER);
+
+            if (token != null && token.startsWith(JwtProperties.ACCESS_TOKEN_PREFIX)) {
+                token = token.substring(JwtProperties.ACCESS_TOKEN_PREFIX.length());
+            }
+
+            AuthenticateMember authenticateMember = jwtParser.getAuthenticateMember(token);
+
+            Optional<Member> member = memberRepository.findByUsername(authenticateMember.username());
+            Optional<OauthMember> oauthMember = oauthMemberRepository.findById(authenticateMember.id());
+
+            nickname = member
+                    .map(Member::getNickname)
+                    .orElseGet(() -> oauthMember
+                            .map(OauthMember::getNickname)
+                            .orElseThrow(SignInErrorCode.NOT_FOUND_USERNAME::defaultException));
+            memberId = authenticateMember.id();
         }
 
-        AuthenticateMember authenticateMember = jwtParser.getAuthenticateMember(token);
-
-        Optional<Member> member = memberRepository.findByUsername(authenticateMember.username());
-
-        Optional<OauthMember> oauthMember = oauthMemberRepository.findById(authenticateMember.id());
-
-        String nickname = member
-                .map(Member::getNickname)
-                .orElseGet(() -> oauthMember
-                        .map(OauthMember::getNickname)
-                        .orElseThrow(SignInErrorCode.NOT_FOUND_USERNAME::defaultException));
-
-
-        ChatMessage chatMessage = chatMessageDtoMapper.convertToEntity(message, MessageType.TALK, nickname, authenticateMember.id());
+        ChatMessage chatMessage = chatMessageDtoMapper.convertToEntity(message, MessageType.TALK, nickname,memberId);
         chatMessageRepository.save(chatMessage);
 
         // @SendTo 없이 메서드 내에서 직접 전송
